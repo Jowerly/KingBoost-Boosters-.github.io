909aecc895c82a595a1f8c858a3a698db2e87527
Jowerly <joseluisfloresarapa16@gmail.com>
2024-09-05 16:13:41 -0500

    eqweqe

---
+++ Functions.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Cambia la barra de navegación cuando haces scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
